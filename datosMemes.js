e3076a6c8a6b527815fb32468637be84b8c707b7
Estudiante de Desarrollo de Software <71363459+JoAlejo09@users.noreply.github.com>
2025-04-28 15:53:57 -0500

    Commit

--- datosMemes.js
+++ datosMemes.js
@@ -1,19 +1,27 @@
 const obtenerMemes = async () => {
+    const request = await fetch("https://api.imgflip.com/get_memes"); 
+    const response = await request.json()
+    console.log(response)
 
     const memesContainers = document.getElementById('list-memes');
     const memeTemplate = document.getElementById('meme'); 
-
-    const request = await fetch("https://api.imgflip.com/get_memes"); 
-
-    const respuesta = await request.json();
-
-    respuesta.data.memes.slice(0, 8).forEach(meme => {
-        const newMemeCard = memeTemplate.cloneNode(true);
-        const img = newMemeCard.querySelector('img');
-        img.src = meme.url; 
-        memesContainers.appendChild(newMemeCard);
-    });
-    memeTemplate.remove();
-};
+    
+    response.data.memes.slice(0,8).forEach(meme=>{
+        const newMemeCard = memeTemplate.cloneNode(true)
+        const img = newMemeCard.querySelector("img")
+        img.src = meme.url
+        memesContainers.appendChild(newMemeCard)
+    })
+    memeTemplate.remove()
+}
+const obtenerGif = async() =>{
+    const request = await fetch("https://api.giphy.com/v1/stickers/search?api_key=lerx9FDWD5PH78M207Cj95ac4KTt8G3v&q=cr7&limit=2")
+    const response = await request.json()
+    console.log(response.data[0].images.original.url)
+    const gifTemplate= document.getElementById('gif');
+    gifTemplate.src= response.data[0].images.original.url;
+    
+}
 
 obtenerMemes();
+obtenerGif();
